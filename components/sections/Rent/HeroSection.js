563046634206c0f720b5311f12959b14a5a5bca7
Vovchik4 <ch.vova321@gmail.com>
2021-07-10 22:47:22 +0300

    feat: breadcrumbs animate

--- components/sections/Rent/HeroSection.js
+++ components/sections/Rent/HeroSection.js
@@ -1,9 +1,35 @@
 import { useRouter } from "next/router";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import urls from "../../../urls";
 import BreadCrumbs from "../../BreadCrumbs";
+import { useInView } from "react-intersection-observer";
+import { motion, useAnimation } from "framer-motion";
 
 export default function HeroSection() {
+  // Animation into scroll block]
+  const [refScroll, intView] = useInView(); // { threshold: 0.1 }
+  const animation = useAnimation();
+  const animationText = useAnimation();
+
+  useEffect(() => {
+    if (intView) {
+      animation.start({
+        width: "100%",
+      });
+      animationText.start({
+        scale: 1,
+        opacity: 1,
+      });
+    }
+    if (!intView) {
+      animation.start({ width: "0" });
+      animationText.start({
+        scale: 0,
+        opacity: 0,
+      });
+    }
+  }, [intView]);
+
   const router = useRouter();
   let currUrl = router.route;
 
@@ -42,21 +68,59 @@ export default function HeroSection() {
     <article className="container-main mx-auto px-4 xl:px-0">
       <BreadCrumbs crumbs={crumbs} selected={selected} />
       {currUrl === "/rent/[id]" ? (
-        <div className="grid grid-cols-2 text-lg my-5">
+        <motion.div
+          ref={refScroll}
+          initial={{ width: 0 }}
+          animate={animation}
+          transition={{ duration: 2 }}
+          className="grid grid-cols-2 text-lg my-5"
+        >
           <div className="br-arrow-right relative bg-red-600 text-white text-center py-4">
-            Вибір
+            <motion.div
+              initial={{ scale: 0, opacity: 0 }}
+              animate={animationText}
+              transition={{ delay: 2 }}
+            >
+              Вибір
+            </motion.div>
           </div>
-          <div className="bg-gray-100 text-center py-4">Бронювання</div>
-        </div>
+          <div className="bg-gray-100 text-center py-4">
+            <motion.div
+              initial={{ scale: 0, opacity: 0 }}
+              animate={animationText}
+              transition={{ delay: 2 }}
+            >
+              Бронювання
+            </motion.div>
+          </div>
+        </motion.div>
       ) : (
-        <div className="grid grid-cols-2 text-lg my-5">
+        <motion.div
+          ref={refScroll}
+          initial={{ width: 0 }}
+          animate={animation}
+          transition={{ duration: 2 }}
+          className="grid grid-cols-2 text-lg my-5"
+        >
           <div className="br-arrow-right-white relative bg-gray-100 text-center py-4">
-            Вибір
+            <motion.div
+              initial={{ scale: 0, opacity: 0 }}
+              animate={animationText}
+              transition={{ delay: 2 }}
+            >
+              Вибір
+            </motion.div>
           </div>
           <div className="bg-red-600 text-center text-white py-4">
-            Бронювання
+            <motion.div
+              initial={{ scale: 0, opacity: 0 }}
+              animate={animationText}
+              transition={{ delay: 2 }}
+            >
+              Бронювання
+            </motion.div>
           </div>
-        </div>
+        </motion.div>
       )}
     </article>
   );
